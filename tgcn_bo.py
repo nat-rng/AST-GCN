2221a1105b8f53a193cbec9a6af839c09e9e59d5
nat_rng <n.roongjirarat@gmail.com>
2024-01-14 21:16:28 +0000

    update code

--- tgcn_bo.py
+++ tgcn_bo.py
@@ -1,14 +1,13 @@
-import os
 import pickle
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from kerastuner.tuners import BayesianOptimization
-from sklearn.model_selection import TimeSeriesSplit
 from tgcn_model import TGCNModel
-from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.model_selection import TimeSeriesSplit
+from skopt import gp_minimize
+from skopt.space import Real, Integer, Categorical
+from skopt.utils import use_named_args
 
-# GPU setup
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
     try:
@@ -17,69 +16,50 @@ if gpus:
     except RuntimeError as e:
         print(e)
 
-# Load data
 with open("data/timestep_24/trainX_timestep_24_20240108.pkl", 'rb') as file:
-    trainX = pickle.load(file)
+    trainX_loaded = pickle.load(file)
 
 with open("data/timestep_24/trainY_timestep_24_20240108.pkl", 'rb') as file:
-    trainY = pickle.load(file)
-
-with open("data/timestep_24/testX_timestep_24_20240108.pkl", 'rb') as file:
-    testX = pickle.load(file)
-
-with open("data/timestep_24/testY_timestep_24_20240108.pkl", 'rb') as file:
-    testY = pickle.load(file)
+    trainY_loaded = pickle.load(file)
 
 road_adj = pd.read_excel("road_connection.xlsx")
 adj = np.mat(road_adj)
 
+trainX = np.array(trainX_loaded)
+trainY = np.array(trainY_loaded)
+
 num_nodes = adj.shape[0]
 pre_len = 12
 
-def build_model(hp):
-    gru_units = hp.Choice('gru_units', values=[16, 32, 64, 128])
-    l1 = hp.Float('l1', min_value=0.001, max_value=1, sampling='log')
-    l2 = hp.Float('l2', min_value=0.001, max_value=1, sampling='log')
-    batch_size = hp.Choice('batch_size', values=[16, 32, 64, 128])
-    epochs = hp.Int('epochs', min_value=10, max_value=100, step=10)
+space = [
+    Integer(16, 256, name="gru_units"),
+    Real(0.001, 1, "log-uniform", name="l1"),
+    Real(0.001, 1, "log-uniform", name="l2"),
+    Integer(10, 100, name="epochs"),
+    Categorical([16, 32, 64, 128], name="batch_size")
+]
 
-    model = TGCNModel(num_nodes, gru_units, adj, pre_len, l1, l2)
-    model.compile(optimizer='adam', loss='mse')
-
-    return model, batch_size, epochs
-
-tuner = BayesianOptimization(
-    build_model,
-    objective='val_loss',
-    max_trials=10,
-    executions_per_trial=1,
-    directory='bayesian_optimization',
-    project_name='tgcn_model_tuning'
-)
-
-tscv = TimeSeriesSplit(n_splits=3)
-for trial in tuner.oracle.create_trials(max_trials=10):
-    hp = trial.hyperparameters
-    batch_size = hp.get('batch_size')
-    epochs = hp.get('epochs')
-
+@use_named_args(space)
+def optimize_tgcn(gru_units, l1, l2, epochs, batch_size):
     val_losses = []
 
-    for train_indices, val_indices in tscv.split(trainX):
+    tscv = TimeSeriesSplit(n_splits=3)
+    for train_indices, test_indices in tscv.split(trainX):
         x_train, y_train = trainX[train_indices], trainY[train_indices]
-        x_val, y_val = trainX[val_indices], trainY[val_indices]
+        x_val, y_val = trainX[test_indices], trainY[test_indices]
 
-        model = build_model(hp)
-        early_stopping = EarlyStopping(monitor='val_loss', patience=5, mode='min', verbose=1)
+        model = TGCNModel(num_nodes, gru_units, adj, pre_len, l1, l2)
+        model.compile(optimizer='adam', loss='mse')
         history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size,
-                            validation_data=(x_val, y_val), callbacks=[early_stopping], verbose=0)
-        val_losses.append(min(history.history['val_loss']))
+                            validation_data=(x_val, y_val), verbose=0)
 
-    avg_val_loss = np.mean(val_losses)
-    tuner.oracle.update_trial(trial.trial_id, {'val_loss': avg_val_loss})
-    tuner.save_model(trial.trial_id, model)
+        val_losses.append(history.history['val_loss'][-1])
 
-best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+    return np.mean(val_losses)
 
-with open('best_hyperparameters_keras_tuner.pkl', 'wb') as f:
-    pickle.dump(best_hps.values, f)
+res = gp_minimize(optimize_tgcn, space, n_calls=50, random_state=42)
+best_hyperparameters = {dim.name: res.x[i] for i, dim in enumerate(space)}
+print("Best hyperparameters: ", best_hyperparameters)
+
+with open('best_hyperparameters_bayesian_opt.pkl', 'wb') as f:
+    pickle.dump(best_hyperparameters, f)
